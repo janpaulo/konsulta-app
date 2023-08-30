cc923a6db6d34b0e06d23569bfedfbcc97b144ab
janpaulo <janpaulobinaluyo@gmail.com>
2023-08-30 05:13:05 +0000

    add function

--- src/pages/konsulta/mainKonsultaForm.js
+++ src/pages/konsulta/mainKonsultaForm.js
@@ -7,22 +7,51 @@ class mainKonsultaForm extends React.Component {
     super();
     this.state = {
       title: "Konsulta Register Form",
+      konsultaItem: {},
       items: [],
-    };
-    // function name(params) {
-    //   ''
-    // }
+    }
+    
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this)
   }
 
+  
+  handleSubmit(params) {
+    console.log("here me")
+      this.setState({
+        bank: {
+          ...this.state.bank,
+          name: e.target.value
+        }
+      })
+    
+  }
+
+  handleInputChange(e) {
+    console.log(e.target.value)
+    this.setState({
+      konsultaItem: {
+        ...this.state.konsultaItem,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+
+
   render() {
+    console.log(this.state.konsultaItem.Label1)
     return (
       <>
         <Typography variant="h5" component="h5">
           {this.state.title}
+          {this.state.konsultaItem.Label1}
         </Typography>
-				<div>
-					<Forms />
-				</div>
+        <div>
+          <Forms
+            handleClick={this.handleSubmit}
+            onchange={this.handleInputChange}
+          />
+        </div>
       </>
     );
   }
